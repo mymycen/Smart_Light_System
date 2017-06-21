c241179056008e808d3aaa5da6d0f2b688390162
leolileo <l.li@campus.tu-berlin.de>
2017-06-21 21:28:09 +0200

    starting the gui

---
+++ src/main/resources/assets/app/navbarController.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Leo on 21.06.2017.
+ */
+(function() {
+    'use strict';
+
+    var exampleApp = angular.module('exampleApp');
+
+    /**
+     * This controller calls 'rooms' and 'devices' request handlers which are registered in the ExampleApp.java.
+     */
+    exampleApp.controller('navbarController', [ '$scope', '$http', function($scope, $http) {
+
+    } ]);
+})();
