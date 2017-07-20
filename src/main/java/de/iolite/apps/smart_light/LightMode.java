913f21d2ddd0ebb58bf98d992acec1feee211eba
leolileo <l.li@campus.tu-berlin.de>
2017-07-20 05:03:15 +0200

    gui stuff

--- src/main/java/de/iolite/apps/smart_light/LightMode.java
+++ src/main/java/de/iolite/apps/smart_light/LightMode.java
@@ -1,34 +1,50 @@
 package de.iolite.apps.smart_light;
 
 public class LightMode {
-	
-	int dimmLevel;
-	double hue;
-	double saturation;
-	public LightMode(int dimmLevel, double hue, double saturation){
-		this.dimmLevel = dimmLevel;
-		this.hue = hue;
-		this.saturation = saturation;
-	}
-	public int getDimmLevel() {
-		return dimmLevel;
-	}
-	public void setDimmLevel(int dimmLevel) {
-		this.dimmLevel = dimmLevel;
-	}
-	public double getHue() {
-		return hue;
-	}
-	public void setHue(double hue) {
-		this.hue = hue;
-	}
-	public double getSaturation() {
-		return saturation;
-	}
-	public void setSaturation(double saturation) {
-		this.saturation = saturation;
-	}
-	
-	
+
+    String modeName;
+    int dimmLevel;
+    double hue;
+    double saturation;
+
+    public LightMode(int dimmLevel, double hue, double saturation, String modeName) {
+        this.modeName = modeName;
+        this.dimmLevel = dimmLevel;
+        this.hue = hue;
+        this.saturation = saturation;
+    }
+
+    public String getModeName() {
+        return modeName;
+    }
+
+    public void setModeName(String modeName) {
+        this.modeName = modeName;
+    }
+
+    public int getDimmLevel() {
+        return dimmLevel;
+    }
+
+    public void setDimmLevel(int dimmLevel) {
+        this.dimmLevel = dimmLevel;
+    }
+
+    public double getHue() {
+        return hue;
+    }
+
+    public void setHue(double hue) {
+        this.hue = hue;
+    }
+
+    public double getSaturation() {
+        return saturation;
+    }
+
+    public void setSaturation(double saturation) {
+        this.saturation = saturation;
+    }
+
 
 }
--- src/main/java/de/iolite/apps/smart_light/RequestHandlers/setPropertyRequestHandler.java
+++ src/main/java/de/iolite/apps/smart_light/RequestHandlers/setPropertyRequestHandler.java
@@ -11,7 +11,6 @@ import de.iolite.common.requesthandler.IOLITEHTTPRequest;
 import de.iolite.common.requesthandler.IOLITEHTTPResponse;
 import de.iolite.common.requesthandler.IOLITEHTTPStaticResponse;
 import de.iolite.drivers.basic.DriverConstants;
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.slf4j.Logger;
@@ -27,52 +26,49 @@ public class setPropertyRequestHandler extends FrontendAPIRequestHandler {
     private Logger LOGGER;
     private DeviceAPI deviceAPI;
     private EnvironmentAPI environmentAPI;
-    private processHttp processor = new processHttp ();
+    private processHttp processor = new processHttp();
 
-    public setPropertyRequestHandler(Logger LOGGER, DeviceAPI deviceAPI, EnvironmentAPI environmentAPI){
+    public setPropertyRequestHandler(Logger LOGGER, DeviceAPI deviceAPI, EnvironmentAPI environmentAPI) {
 
         this.LOGGER = LOGGER;
         this.deviceAPI = deviceAPI;
         this.environmentAPI = environmentAPI;
     }
 
-        @Override
-        protected IOLITEHTTPResponse handleRequest(final IOLITEHTTPRequest request, final String subPath) {
-            String value = "false";
-            String deviceType;
+    @Override
+    protected IOLITEHTTPResponse handleRequest(final IOLITEHTTPRequest request, final String subPath) {
+        String value = "false";
+        String deviceType;
 
+        try {
+            deviceType = new JSONObject(processor.readPassedData(request)).getString("deviceType");
+            LOGGER.info("TYPPEEEE  " + deviceType);
+        } catch (final JSONException e) {
+            LOGGER.error("Could not handle devices request due to a JSON error: {}", e.getMessage(), e);
+            return new IOLITEHTTPStaticResponse(e.getMessage(), HTTPStatus.BadRequest, "text/plain");
+        } catch (final IOException e) {
+            LOGGER.error("Could not handle devices request due to an I/O error: {}", e.getMessage(), e);
+            return new IOLITEHTTPStaticResponse(e.getMessage(), HTTPStatus.BadRequest, "text/plain");
+        }
 
-            try {
-                //propertyType = new JSONObject(readPassedData(request)).getString("propertyType");
-                deviceType = new JSONObject(processor.readPassedData(request)).getString("deviceType");
-                LOGGER.info("TYPPEEEE  " + deviceType    );
-            } catch (final JSONException e) {
-                LOGGER.error("Could not handle devices request due to a JSON error: {}", e.getMessage(), e);
-                return new IOLITEHTTPStaticResponse(e.getMessage(), HTTPStatus.BadRequest, "text/plain");
-            } catch (final IOException e) {
-                LOGGER.error("Could not handle devices request due to an I/O error: {}", e.getMessage(), e);
-                return new IOLITEHTTPStaticResponse(e.getMessage(), HTTPStatus.BadRequest, "text/plain");
-            }
-
-            final JSONArray jsonDeviceArray = new JSONArray();
-            for (final Device device : deviceAPI.getDevices()) {
-                if (device.getIdentifier().equals(deviceType)){
-                    DeviceBooleanProperty onProperty = device.getBooleanProperty(DriverConstants.PROPERTY_on_ID);
-                    try {
-                        if(!onProperty.getValue()){
-                            onProperty.requestValueUpdate(true);}
-                        else {
-                            onProperty.requestValueUpdate(false);
-                        }
-                        value = onProperty.getValue().toString();
-                    }catch (Exception e){
-                        LOGGER.error(e.toString());
+        for (final Device device : deviceAPI.getDevices()) {
+            if (device.getIdentifier().equals(deviceType)) {
+                DeviceBooleanProperty onProperty = device.getBooleanProperty(DriverConstants.PROPERTY_on_ID);
+                try {
+                    if (!onProperty.getValue()) {
+                        onProperty.requestValueUpdate(true);
+                    } else {
+                        onProperty.requestValueUpdate(false);
                     }
+                    value = onProperty.getValue().toString();
+                } catch (Exception e) {
+                    LOGGER.error(e.toString());
                 }
             }
-
-            final JSONObject response = new JSONObject();
-            response.put("value", value);
-            return new IOLITEHTTPStaticResponse(response.toString(), IOLITEHTTPResponse.JSON_CONTENT_TYPE);
         }
+
+        final JSONObject response = new JSONObject();
+        response.put("value", value);
+        return new IOLITEHTTPStaticResponse(response.toString(), IOLITEHTTPResponse.JSON_CONTENT_TYPE);
     }
+}
